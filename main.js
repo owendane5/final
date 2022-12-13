a135a1bd34da9febfc4e7a5965b0707066a7075b
owendane5 <112992614+owendane5@users.noreply.github.com>
2022-12-12 19:31:58 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log("JavaScript Portion of the Test");
+
+// Step 1: Write a multi-line comment detailing your name and student number
+/* 
+Owen Dane
+200529027
+*/
+
+// Step 2: Create a block scoped, immutable variable that stores an empty array
+
+const arr = [];
+
+// Step 3: Add the 3 people's names to the array
+
+
+arr.push("John", "Josh", "Jerry");
+
+
+// Step 4: Remove the second array item from the array
+
+//const result = arr.splice (1,1);
+
+
+// Step 5: Using array map, iterate over the array items and make them all uppercase. Store the result in a new, block scoped, mutable variable
+
+
+//const result = arr.map(function (arrayItem)) {
+    //return arrayItem.toUpperCase();
+
+
+// Step 6: Create an event listener that fires a callback function when the aside h2 is clicked. It should alert "Hello from Boo the World's Cutest Dog!";
+
